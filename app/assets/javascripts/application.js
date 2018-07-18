75c090f7aa97cf2c524af15914fdee746c438aad
David Bosveld <david@wetransfer.com>
2018-07-18 15:35:01 +0200

    Changed style to vanilla instead of metro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require rails-ujs
 //= require jquery
-//= require activestorage
 //= require turbolinks
-//= require_tree ./metro
+
 
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,37 +1,12 @@
-# SimpleForm.setup do |config|
-#   config.wrappers :default, class: :input,
-#     hint_class: :field_with_hint, error_class: :field_with_errors, valid_class: :field_without_errors do |b|
-#     b.use :html5
-#     b.use :placeholder
-#     b.optional :maxlength
-#     b.optional :minlength
-#     b.optional :pattern
-#     b.optional :min_max
-#     b.optional :readonly
-#     b.use :label_input
-#     b.use :hint,  wrap_with: { tag: :span, class: :hint }
-#     b.use :error, wrap_with: { tag: :span, class: :error }
-#   end
-
-#   config.default_wrapper = :default
-
-#   config.boolean_style = :nested
-
-#   config.button_class = 'btn'
-
-#   config.error_notification_tag = :div
-
-#   config.error_notification_class = 'error_notification'
-
-#   config.browser_validations = false
-#   config.boolean_label_class = 'checkbox'
-# end
-
+# Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.error_notification_class = 'error'
-  config.button_class = 'button'
+  config.error_notification_class = 'alert alert-danger'
+  config.button_class = 'btn'
+  config.boolean_label_class = nil
+  config.boolean_style = :inline
+  config.item_wrapper_tag = :p
 
-  config.wrappers :metro_form, tag: 'div' do |b|
+  config.wrappers :vanilla_form, tag: 'div', class: 'p-form-validation', error_class: 'is-error' do |b|
     b.use :html5
     b.use :placeholder
     b.optional :maxlength
@@ -39,26 +14,105 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
     b.use :label
-    b.use :input
-    b.use :error, wrap_with: { tag: 'div', class: 'message' }
-    b.wrapper tag: :span, class: 'tooltip', unless_blank: true do |component|
-      component.optional :hint,  wrap_with: { tag: :span, class: 'tooltip__content' }
+    b.use :input, class: 'p-form-validation__input'
+    b.use :error, wrap_with: { tag: 'p', class: 'p-form-validation__message red-text' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+  end
+
+  config.wrappers :vanilla_text, tag: 'div', class: 'p-form-validation ', error_class: 'is-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label
+    b.use :input, class: 'p-form-validation__input'
+    b.use :error, wrap_with: { tag: 'p', class: 'p-form-validation__message red-text' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+  end
+
+  config.wrappers :vanilla_boolean, tag: 'div', class: '', error_class: 'is-error' do |b|
+    b.use :html5
+    b.optional :readonly
+    b.use :label
+    b.use :input, class: 'p-form-validation__input'
+
+    b.use :error, wrap_with: { tag: 'p', class: 'p-form-validation__message red-text' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+  end
+
+  config.wrappers :vanilla_toggle, tag: 'p', class: ' switch', error_class: 'is-error' do |b|
+    b.use :html5
+    b.optional :readonly
+
+    b.use :label
+    b.wrapper tag: 'label' do |ba|
+      ba.use :input, class: 'p-form-validation__input'
+      ba.use :tag, tag: 'span', class: 'lever'
+    end
+
+    b.use :error, wrap_with: { tag: 'p', class: 'p-form-validation__message red-text' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+  end
+
+  config.wrappers :vanilla_radio_and_checkboxes, tag: 'div', class: '', error_class: 'is-error' do |b|
+    b.use :html5
+    b.optional :readonly
+    b.use :label
+    b.use :input, class: 'p-form-validation__input'
+    b.use :error, wrap_with: { tag: 'p', class: 'p-form-validation__message red-text' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+  end
+
+  config.wrappers :vanilla_file_input, tag: 'div', class: 'file-field p-form-validation ', error_class: 'is-error' do |b|
+    b.use :html5
+
+    b.wrapper tag: :div, class: 'btn' do |ba|
+      ba.use :tag, tag: :span, text: :label_text
+      ba.use :input
+    end
+
+    b.wrapper tag: :div, class: 'file-path-wrapper' do |ba|
+      ba.use :tag, tag: :input, class: 'file-path validate', type: 'text', placeholder: 'Browse...'
+    end
+
+    b.use :error, wrap_with: { tag: 'p', class: 'p-form-validation__message red-text' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+  end
+
+  config.wrappers :vanilla_multiple_file_input, tag: 'div', class: 'file-field p-form-validation ', error_class: 'is-error' do |b|
+    b.use :html5
+
+    b.wrapper tag: :div, class: 'btn' do |ba|
+      ba.use :tag, tag: :span, text: :label_text
+      ba.use :input, multiple: true
+    end
+
+    b.wrapper tag: :div, class: 'file-path-wrapper' do |ba|
+      ba.use :tag, tag: :input, class: 'file-path validate', type: 'text', placeholder: 'Upload one or more files'
+    end
+
+    b.use :error, wrap_with: { tag: 'p', class: 'p-form-validation__message red-text' }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+  end
+
+  config.wrappers :vanilla_slider, tag: 'div', class: 'p-form-validation', error_class: 'is-error' do |b|
+    b.use :html5
+
+    b.wrapper tag: :div, class: 'p-slider_wrapper' do |ba|
+      ba.use :tag, tag: :input, class: 'p-slider', type: 'range'
+      ba.use :tag, tag: :input, class: 'p-slider__input', type: 'text'
     end
   end
 
-  config.wrappers :metro_checkbox do |b|
-    b.use :html5
-    b.use :input, type: 'checkbox', 'data-role': 'checkbox'
-  end
-
-
-  config.default_wrapper = :metro_form
+  config.default_wrapper = :vanilla_form
   config.wrapper_mappings = {
-    text: :metro_form,
-    check_boxes: :metro_checkbox,
-    radio_buttons: :metro_form,
-    file: :metro_form,
-    boolean: :metro_form,
-    slider: :metro_form
+    text: :vanilla_text,
+    check_boxes: :vanilla_radio_and_checkboxes,
+    radio_buttons: :vanilla_radio_and_checkboxes,
+    file: :vanilla_file_input,
+    boolean: :vanilla_boolean,
+    slider: :vanilla_slider
   }
 end
