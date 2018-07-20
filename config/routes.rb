85c48c784c684a24d22fec528eb06100caad8753
David Bosveld <david@wetransfer.com>
2018-07-20 10:53:02 +0200

    sign_out method through get route

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, path: '/', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    get 'sign_in',     to: 'devise/sessions#new',     as: :new_user_session
-    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get     'sign_in',                to: 'devise/sessions#new',      as: :new_user_session
+    get     'sign_out',               to: 'devise/sessions#destroy',  as: :destroy_user_session
   end
-  get '/sign-up', as: 'signup', to: 'page#signup'
+  get '/sign-up',                     to: 'page#signup',              as: 'signup'
 
   authenticated :user do
-    get '/dashboard', to: 'dashboard#show', as: 'dashboard'
+    get '/dashboard',                 to: 'dashboard#show', as: 'dashboard'
     get '/dashboard/playlist_tracks', to: 'dashboard#playlist_info'
-    post '/share_playlist', to: 'dashboard#export_to_transfer'
-    post '/share_selected_playlist', to: 'dashboard#export_selected_playlists'
-    post '/share_selected_tracks', to: 'dashboard#export_selected_tracks'
-    get '/dashboard/top_tracks', to: 'dashboard#top_tracks'
+    post '/share_playlist',           to: 'dashboard#export_to_transfer'
+    post '/share_selected_playlist',  to: 'dashboard#export_selected_playlists'
+    post '/share_selected_tracks',    to: 'dashboard#export_selected_tracks'
+    get '/dashboard/top_tracks',      to: 'dashboard#top_tracks'
   end
 
   root 'page#home'
